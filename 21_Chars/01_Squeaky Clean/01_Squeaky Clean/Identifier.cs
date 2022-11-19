be4348563858c84539cc6eade5657db9c853afc5
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-11-19 14:02:27 +0200

    First two task in Squeaky Clean

---
+++ 21_Chars/01_Squeaky Clean/01_Squeaky Clean/Identifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        string cleanedIdentifier = "";
+
+        foreach (char character in identifier)
+        {
+            if (character.ToString() == " ")
+            {
+                cleanedIdentifier = identifier.Replace(character, '_');
+            }
+
+            if (char.IsControl(character))
+            {
+                cleanedIdentifier = identifier.Replace(character.ToString(), "CTRL");
+            }
+        }
+
+        return cleanedIdentifier;
+    }
+}
---
+++ 21_Chars/01_Squeaky Clean/01_Squeaky Clean/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        // Task 1
+        Helper.DisplayTask("I", "Replace any spaces encountered with underscores");
+        Helper.DisplayExample(
+            "Identifier.Clean(\"my   Id\");",
+            "my___Id");
+
+        Console.WriteLine(Identifier.Clean("my   Id"));
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Replace control characters with the upper case string \"CTRL\"");
+        Helper.DisplayExample(
+            "Identifier.Clean(\"my\\0Id\");",
+            "myCTRLId");
+
+        Console.WriteLine(Identifier.Clean("my\0Id"));
+        Console.WriteLine();
+    }
+}
