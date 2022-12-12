954465c73d534728e468579f14ca2b75019fd1f6
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-12-12 07:04:14 +0200

    Folder Tweeks

---
+++ 22_String Builder/01_Squeaky Clean/01_Squeaky Clean/Identifier.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+
+public static class Identifier
+{
+    private static bool IsGreekLowerCase(char c) => (c > 'α' && c <= 'ω');
+
+    public static string Clean(string identifier)
+    {
+        StringBuilder sb = new StringBuilder();
+        bool isAfterDash = false;
+
+        foreach (char c in identifier)
+        {
+            sb.Append(c switch
+            {
+                _ when IsGreekLowerCase(c) => default,
+                _ when isAfterDash => Char.ToUpperInvariant(c),
+                _ when Char.IsLetter(c) => c,
+                _ when Char.IsControl(c) => "CTRL",
+                _ when Char.IsWhiteSpace(c) => '_',
+                _ => default,
+            });
+
+            isAfterDash = c.Equals('-');
+        }
+        
+        return sb.ToString();
+    }
+}
---
+++ 22_String Builder/01_Squeaky Clean/01_Squeaky Clean/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine(Console.OutputEncoding);
+
+        // Task 1
+        Helper.DisplayTask("I", "Replace any spaces encountered with underscores");
+        Helper.DisplayExample(
+            "Identifier.Clean(\"my   Id\");",
+            "my___Id");
+
+        Console.WriteLine(Identifier.Clean("my   Id"));
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Replace control characters with the upper case string \"CTRL\"");
+        Helper.DisplayExample(
+            "Identifier.Clean(\"my\\0Id\");",
+            "myCTRLId");
+
+        Console.WriteLine(Identifier.Clean("my\0Id"));
+        Console.WriteLine();
+
+
+
+        // Task 3
+        Helper.DisplayTask("III", "Convert kebab-case to camelCase");
+        Helper.DisplayExample(
+            "Identifier.Clean(\"à-ḃç\");",
+            "àḂç");
+
+        Console.WriteLine(Identifier.Clean("à-ḃç"));
+        Console.WriteLine();
+
+
+
+        // Task 4
+        Helper.DisplayTask("IV", "Omit characters that are not letters");
+        Helper.DisplayExample(
+            "Identifier.Clean(\"1😀2😀3😀\");",
+            "\"\""
+            );
+        Console.WriteLine(Identifier.Clean("1😀2😀3😀"));
+        Console.WriteLine();
+
+
+
+        // Task 5
+        Helper.DisplayTask("V", "Omit Greek lower case letters");
+        Helper.DisplayExample(
+            "Identifier.Clean(\"MyΟβιεγτFinder\");",
+            "MyΟFinder"
+            );
+        Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
+        Console.WriteLine();
+    }
+}
