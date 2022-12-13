33862353c6c9f376d438771ae55605ab73aab0be
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-12-13 15:40:42 +0200

    Solved Booking up for Beauty

---
+++ 23_Datetimes/01_ Booking up for Beauty/01_ Booking up for Beauty/Appointment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+static class Appointment
+{
+    public static DateTime Schedule(string appointmentDateDescription)
+    {
+        DateTime appointment = DateTime.Parse(appointmentDateDescription);
+        return appointment;
+    }
+
+    public static bool HasPassed(DateTime appointmentDate)
+    {
+        return appointmentDate < DateTime.Now;
+    }
+
+    public static bool IsAfternoonAppointment(DateTime appointmentDate)
+    {
+        return appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
+    }
+
+    public static string Description(DateTime appointmentDate)
+    {
+        return $"You have an appointment on {appointmentDate.ToString()}.";
+    }
+
+    public static DateTime AnniversaryDate()
+    {
+        return new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
+    }
+}
---
+++ 23_Datetimes/01_ Booking up for Beauty/01_ Booking up for Beauty/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+internal class BookingUpForBeauty
+{
+    public static void Main(string[] args)
+    {
+        // Task 1
+
+        Helper.DisplayTask("I", "Parse appointment date");
+        Helper.DisplayExample(
+            "Appointment.Schedule(\"7/25/2019 13:45:00\")",
+            new DateTime(2019, 7, 25, 13, 45, 0).ToString()
+            );
+        Console.WriteLine(Appointment.Schedule("7/25/2019 13:45:00"));
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Check if an appointment has already passed");
+        Helper.DisplayExample(
+            "Appointment.HasPassed(new DateTime(1999, 12, 31, 9, 0, 0))",
+            "True"
+            );
+        Console.WriteLine(Appointment.HasPassed(new DateTime(1999, 12, 31, 9, 0, 0)));
+        Console.WriteLine();
+
+
+
+        // Task 3
+        Helper.DisplayTask("III", "Check if appointment is in the afternoon");
+        Helper.DisplayExample(
+            "Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 15, 0, 0))",
+            "True"
+            );
+        Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 15, 0, 0)));
+        Console.WriteLine();
+
+
+
+        // Task 4
+        Helper.DisplayTask("IV", "Describe the time and date of the appointment");
+        Helper.DisplayExample(
+            "Appointment.Description(new DateTime(2019, 03, 29, 15, 0, 0))",
+            "You have an appointment on 3/29/2019 3:00:00 PM.");
+        Console.WriteLine(Appointment.Description(new DateTime(2019, 03, 29, 15, 0, 0)));
+        Console.WriteLine();
+
+
+
+        // Task 5
+        Helper.DisplayTask("V", "Return the anniversary date");
+        Helper.DisplayExample(
+            "Appointment.AnniversaryDate()",
+            new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0).ToString()
+            );
+        Console.WriteLine(Appointment.AnniversaryDate());
+        Console.WriteLine();
+    }
+}
