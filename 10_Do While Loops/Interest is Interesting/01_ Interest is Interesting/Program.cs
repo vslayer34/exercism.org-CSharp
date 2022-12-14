2f0aff9ac6299011fa9420ab652882736204f9f9
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-12-14 09:33:57 +0200

    Solved  Interest is Interesting

---
+++ 10_Do While Loops/Interest is Interesting/01_ Interest is Interesting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        // Task 1
+        Helper.DisplayTask("I", "Calculate the interest rate");
+        Helper.DisplayExample(
+            "SavingsAccount.InterestRate(balance: 200.75m)",
+            0.5f.ToString()
+            );
+        Console.WriteLine(SavingsAccount.InterestRate(balance: 200.75m));
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Calculate the interest");
+        Helper.DisplayExample(
+            "SavingsAccount.Interest(balance: 200.75m)",
+            1.00375m.ToString()
+            );
+        Console.WriteLine(SavingsAccount.Interest(balance: 200.75m));
+        Console.WriteLine();
+
+
+
+        // Task 3
+        Helper.DisplayTask("III", "Calculate the annual balance update");
+        Helper.DisplayExample(
+            "SavingsAccount.AnnualBalanceUpdate(balance: 200.75m)",
+            201.75375m.ToString()
+            );
+        Console.WriteLine(SavingsAccount.AnnualBalanceUpdate(balance: 200.75m));
+        Console.WriteLine();
+
+
+
+        // Task 4
+        Helper.DisplayTask("IV", "Calculate the years before reaching the desired balance");
+        Helper.DisplayExample(
+            "SavingsAccount.YearsBeforeDesiredBalance(balance: 200.75m, targetBalance: 214.88m)",
+            14.ToString()
+            );
+        Console.WriteLine(SavingsAccount.YearsBeforeDesiredBalance(balance: 200.75m, targetBalance: 214.88m));
+        Console.WriteLine();
+    }
+}
---
+++ 10_Do While Loops/Interest is Interesting/01_ Interest is Interesting/SavingsAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance)
+    {
+        if (balance < 0)
+            return 3.213f;
+        else if (balance < 1000)
+            return 0.5f;
+        else if (balance < 5000)
+            return 1.621f;
+        else
+            return 2.475f;
+    }
+
+    public static decimal Interest(decimal balance)
+    {
+        float interestRate = InterestRate(balance);
+        return balance * (decimal)interestRate / 100;
+    }
+
+    public static decimal AnnualBalanceUpdate(decimal balance)
+    {
+        return balance + Interest(balance);
+    }
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        decimal balanceTotal = balance;
+        int numberOfYears = 0;
+        do
+        {
+            balanceTotal = AnnualBalanceUpdate(balanceTotal);
+            numberOfYears++;
+        }
+        while (balanceTotal < targetBalance);
+
+        return numberOfYears;
+    }
+}
