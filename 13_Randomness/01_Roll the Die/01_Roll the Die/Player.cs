f024abf99c07b56f3c48fb8924968cc0d9178bf6
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-09 00:16:53 +0200

    Solved  Roll the Die!

---
+++ 13_Randomness/01_Roll the Die/01_Roll the Die/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Player
+{
+    private Random rand = new Random();
+    private const int numSides = 18;
+
+    public int RollDie()
+    {
+        return rand.Next(1, numSides);
+    }
+
+    public double GenerateSpellStrength()
+    {
+        return Math.Round(rand.NextDouble() * 100, 2);
+    }
+}
---
+++ 13_Randomness/01_Roll the Die/01_Roll the Die/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        Player player = new Player();
+        // Task I
+        Helper.DisplayTask("I", "Enable a wizards and warriors player to roll a die.");
+        Console.WriteLine(player.RollDie());
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Players need their strength. Provide a means to generate spell strength");
+        Console.WriteLine(player.GenerateSpellStrength());
+        Console.WriteLine();
+    }
+}
