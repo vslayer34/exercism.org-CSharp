fb99617b11add161d60dad9c048c2fd6d7eb1b26
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-08 23:28:08 +0200

    Solved Tim from Marketing

---
+++ 09_Nullability/01_Tim from Marketing/01_Tim from Marketing/Badge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {
+        string job = "OWNER";
+
+        if (id == null)
+            return $"{name} - {department?.ToUpper() ?? job}";
+
+        return $"[{id}] - {name} - {department?.ToUpper() ?? job}";
+    }
+}
---
+++ 09_Nullability/01_Tim from Marketing/01_Tim from Marketing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        // Task 1
+        Helper.DisplayTask("I", "Print a badge for an employee");
+        Console.WriteLine(Badge.Print(734, "Ernest Johnny Payne", "Strategic Communication"));
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Print a badge for a new employee");
+        Console.WriteLine(Badge.Print(id: null, "Jane Johnson", "Procurement"));
+        Console.WriteLine();
+
+
+        // Task 3
+        Helper.DisplayTask("III", "Print a badge for the owner");
+        Console.WriteLine(Badge.Print(id: null, "Charlotte Hale", department: null));
+        Console.WriteLine();
+    }
+}
