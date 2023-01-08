fa803f32aeb6e32f9bdcfe6b5aeaaca5403e36ae
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-08 16:37:34 +0200

    Solved International Calling Connoisseur

---
+++ 26_Dictionaries/01_International Calling Connoisseur/01_International Calling Connoisseur/DialingCodes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class DialingCodes
+{
+    public static Dictionary<int, string> GetEmptyDictionary()
+    {
+        return new Dictionary<int, string>();
+    }
+
+    public static Dictionary<int, string> GetExistingDictionary()
+    {
+        return new Dictionary<int, string> 
+        {
+            [1] = "United States of America",
+            [55] = "Brazil",
+            [91] = "India"
+        };
+    }
+
+    public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
+    {
+        return new Dictionary<int, string> { [countryCode] = countryName };
+    }
+
+    public static Dictionary<int, string> AddCountryToExistingDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+    {
+        if (existingDictionary.Count > 0)
+        {
+            existingDictionary.Add(countryCode, countryName);
+        }
+        return existingDictionary;
+    }
+
+    public static string GetCountryNameFromDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode)
+    {
+        if (existingDictionary.ContainsKey(countryCode))
+            return existingDictionary[countryCode];
+        else
+            return string.Empty;
+    }
+
+    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
+    {
+        return existingDictionary.ContainsKey(countryCode);
+    }
+
+    public static Dictionary<int, string> UpdateDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+    {
+        if (!existingDictionary.ContainsKey(countryCode))
+            return existingDictionary;
+        else
+        {
+            existingDictionary[countryCode] = countryName;
+            return existingDictionary;
+        }
+    }
+
+    public static Dictionary<int, string> RemoveCountryFromDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode)
+    {
+        existingDictionary.Remove(countryCode);
+        return existingDictionary;
+    }
+
+    public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
+    {
+        string longestCountryName = string.Empty;
+        foreach (var countryName in existingDictionary.Values)
+        {
+            if (countryName.Length > longestCountryName.Length)
+                longestCountryName = countryName;
+        }
+
+        return longestCountryName;
+    }
+}
---
+++ 26_Dictionaries/01_International Calling Connoisseur/01_International Calling Connoisseur/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        // Task 1
+        Helper.DisplayTask("I", "Create a new dictionary");
+        Dictionary<int, string> newDict = DialingCodes.GetEmptyDictionary();
+        if (newDict != null )
+            Console.WriteLine("New Dicitionary is created");
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Create a pre-populated dictionary");
+        Dictionary<int, string> prePopulatedDict = DialingCodes.GetExistingDictionary();
+        if (prePopulatedDict != null)
+            DisplayDict(prePopulatedDict);
+        Console.WriteLine();
+
+
+
+        // Task 3
+        Helper.DisplayTask("III", "Add a country to an empty dictionary");
+        var emptyDictWithAdd = DialingCodes.AddCountryToEmptyDictionary(44, "United Kingdom");
+        if (emptyDictWithAdd != null)
+            DisplayDict(emptyDictWithAdd);
+        Console.WriteLine();
+
+
+
+        // Task 4
+        Helper.DisplayTask("IV", "Add a country to an existing dictionary");
+        DisplayDict(DialingCodes.AddCountryToExistingDictionary(DialingCodes.GetExistingDictionary(), 44, "Unite Kingdom"));
+        Console.WriteLine();
+
+
+
+        // Task 5
+        Helper.DisplayTask("V", "Get the country name matching a dialing code");
+        Console.WriteLine(DialingCodes.GetCountryNameFromDictionary(DialingCodes.GetExistingDictionary(), 55));
+        Console.WriteLine(DialingCodes.GetCountryNameFromDictionary(DialingCodes.GetExistingDictionary(), 999));
+        Console.WriteLine();
+
+
+
+        // Task 6
+        Helper.DisplayTask("VI", "Check that a country exists in the dictionary");
+        Console.WriteLine(DialingCodes.CheckCodeExists(DialingCodes.GetExistingDictionary(), 55));
+        Console.WriteLine();
+
+
+
+        // Task 7
+        Helper.DisplayTask("VII", "Update a country name");
+        DisplayDict(DialingCodes.UpdateDictionary(DialingCodes.GetExistingDictionary(), 1, "Les États-Unis"));
+        Console.WriteLine();
+        DisplayDict(DialingCodes.UpdateDictionary(DialingCodes.GetExistingDictionary(), 999, "Newlands"));
+        Console.WriteLine();
+
+
+
+        // Task 8
+        Helper.DisplayTask("VIII", "Remove a country from the dictionary");
+        DisplayDict(DialingCodes.RemoveCountryFromDictionary(DialingCodes.GetExistingDictionary(), 91));
+        Console.WriteLine();
+
+
+
+        // Task 9
+        Helper.DisplayTask("IX", "Find the country with the longest name");
+        Console.WriteLine(DialingCodes.FindLongestCountryName(DialingCodes.GetExistingDictionary()));
+        Console.WriteLine();
+    }
+
+
+    public static void DisplayDict(Dictionary<int, string> dict)
+    {
+        foreach (int key in dict.Keys)
+        {
+            Console.WriteLine($"{key} is the Country code of {dict[key]}");
+        }
+    }
+}
