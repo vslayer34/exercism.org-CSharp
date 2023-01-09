0506a5e9a15796510aeb9f251a61c4c73f00af67
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-09 17:06:48 +0200

    Solved Football Match Reports

---
+++ 27_Switch Statements/01_Football Match Reports/01_Football Match Reports/PlayAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class PlayAnalyzer
+{
+    public static string AnalyzeOnField(int shirtNum) => shirtNum switch
+    {
+        1 => "goalie",
+        2 => "left back",
+        3 or 4 => "center back",
+        5 => "right back",
+        >=6 and <=8 => "midfielder",
+        9 => "left wing",
+        10 => "striker",
+        11 => "right wing",
+        _ => throw new ArgumentOutOfRangeException($"{shirtNum} isn't availabe, Please enter a valid number")
+    };
+
+
+    public static string AnalyzeOffField(object report)
+    {
+        return report switch
+        {
+            int supporters => $"There are {supporters} supporters at the match.",
+            string announcement => announcement?.ToString(),
+            Foul foul => foul.GetDescription(),
+            Injury injury => $"Oh no! {injury.GetDescription()} Medics are on the field.",
+            Incident incident => incident.GetDescription(),
+            Manager manager => manager.Club == null ? $"{manager.Name}" : $"{manager.Name} ({manager.Club})",
+            _ => throw new ArgumentException("Invalid parameter eiter int or string")
+        };
+    }
+}
---
+++ 27_Switch Statements/01_Football Match Reports/01_Football Match Reports/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        // Task 1
+        Helper.DisplayTask("I", "Output descriptions of the players based on their shirt number");
+        Console.WriteLine(PlayAnalyzer.AnalyzeOnField(10));
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Raise an alert if an unknown shirt number is encountered");
+        //Console.WriteLine(PlayAnalyzer.AnalyzeOnField(12));
+        Console.WriteLine();
+
+
+
+        // Task 3
+        Helper.DisplayTask("III", "Extend the coverage to include off field activity");
+        Console.WriteLine(PlayAnalyzer.AnalyzeOffField(5000));
+        Console.WriteLine(PlayAnalyzer.AnalyzeOffField("5 minutes to go!"));
+        //Console.WriteLine(PlayAnalyzer.AnalyzeOffField(0.5));
+        Console.WriteLine();
+
+
+
+        // Task 4
+        Helper.DisplayTask("IV", "Report on incidents during the match");
+        Console.WriteLine(PlayAnalyzer.AnalyzeOffField(new Foul()));
+        Console.WriteLine(PlayAnalyzer.AnalyzeOffField(new Injury(8)));
+        Console.WriteLine();
+
+
+
+        // Task 5
+        Helper.DisplayTask("V", "Report on club managers");
+        Console.WriteLine(PlayAnalyzer.AnalyzeOffField(new Manager("José Mário dos Santos Mourinho Félix", null)));
+        Console.WriteLine(PlayAnalyzer.AnalyzeOffField(new Manager("Jürgen Klopp", "Liverpool")));
+        Console.WriteLine();
+    }
+}
