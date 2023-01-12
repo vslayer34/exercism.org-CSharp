31dfd5603fdce6db8063dbfffd18055f9b61ac2b
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-12 16:56:59 +0200

    Solved Logs, Logs, Logs!

---
+++ 28_Enums/01_Logs, Logs, Logs/01_Logs, Logs, Logs/LogLine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+// TODO: define the 'LogLevel' enum
+enum LogLevel
+{
+    Unknown,
+    Trace,
+    Debug,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42
+};
+
+
+static class LogLine
+{
+
+    private static readonly string[] logMessages = { "TRC", "DBG", "INF", "WRN", "ERR", "FTL" };
+    private const string errorMessage = "Please check the log message type for an error";
+
+    public static LogLevel ParseLogLevel(string logLine)
+    {
+        string logLvlString = logLine.Substring(1, 3);
+
+        return logLvlString switch
+        {
+            "TRC" => LogLevel.Trace,
+            "DBG" => LogLevel.Debug,
+            "INF" => LogLevel.Info,
+            "WRN" => LogLevel.Warning,
+            "ERR" => LogLevel.Error,
+            "FTL" => LogLevel.Fatal,
+            _ => LogLevel.Unknown,
+        };
+    }
+
+    public static string OutputForShortLog(LogLevel logLevel, string message)
+    {
+        return $"{(int)logLevel}:{message}";
+    }
+}
---
+++ 28_Enums/01_Logs, Logs, Logs/01_Logs, Logs, Logs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        // Task 1
+        Helper.DisplayTask("I", "Parse log level");
+        Helper.DisplayExample
+        (
+            "LogLine.ParseLogLevel(\"[INF]: File deleted\")",
+            LogLevel.Info.ToString()
+        );
+        Console.WriteLine(LogLine.ParseLogLevel("[INF]: File deleted"));
+        Console.WriteLine();
+
+
+        
+        // Task 2
+        Helper.DisplayTask("II", "Support unknown log level");
+        Helper.DisplayExample
+            (
+                "LogLine.ParseLogLevel(\"[XYZ]: Overly specific, out of context message\")",
+                LogLevel.Unknown.ToString()
+            );
+        Console.WriteLine(LogLine.ParseLogLevel("[XYZ]: Overly specific, out of context message"));
+        Console.WriteLine();
+
+
+
+        // Task 3
+        Helper.DisplayTask("III", "Convert log line to short format");
+        Helper.DisplayExample(
+            "LogLine.OutputForShortLog(LogLevel.Error, \"Stack overflow\")",
+            "6:Stack overflow"
+            );
+        Console.WriteLine(LogLine.OutputForShortLog(LogLevel.Error, "Stack overflow"));
+        Console.WriteLine();
+    }
+}
