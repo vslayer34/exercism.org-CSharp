396631305a994a7da9b7eaca57aabecf9db41b4c
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-13 23:09:16 +0200

    Solved Weighing Machine

---
+++ 31_Properties/01_ Weighing Machine/01_ Weighing Machine/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+// URL: https://exercism.org/tracks/csharp/exercises/weighing-machine
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        // Task 1
+        Helper.DisplayTask("I", "Allow the weighing machine to have a precision");
+        var wm = new WeighingMachine(precision: 3);
+
+        Helper.DisplayExample("wm.Precision",
+            "3"
+            );
+        Console.WriteLine(wm.Precision);
+        Console.WriteLine();
+
+
+
+        // Task 2
+        Helper.DisplayTask("II", "Allow the weight to be set on the weighing machine");
+        wm.Weight = 60.5f;
+        Console.WriteLine(wm.Weight);
+        Console.WriteLine();
+
+
+        // Task 3
+        Helper.DisplayTask("III", "Ensure that a negative input weight is rejected");
+        //wm.Weight = -10;
+
+
+        // Task 4
+        Helper.DisplayTask("IV", "Allow a tare adjustment to be applied to the weighing machine");
+        //wm.TareAdjustment = -10.6f;
+
+
+
+        // Task 5
+        Helper.DisplayTask("V", "Ensure that the weighing machine has a default tare adjustment");
+        Console.WriteLine(wm.TareAdjustment == 5.0f);
+        Console.WriteLine();
+
+
+
+        // Task 6
+        Helper.DisplayTask("VI", "Allow the weight to be retrieved");
+        wm.TareAdjustment = 10;
+        wm.Weight = 60.567f;
+        Helper.DisplayExample(
+            "wm.DisplayWeight == \"50.567 kg\"",
+            "50.567 kg"
+            );
+        Console.WriteLine(wm.DisplayWeight);
+        Console.WriteLine();
+    }
+}
---
+++ 31_Properties/01_ Weighing Machine/01_ Weighing Machine/WeighingMachine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class WeighingMachine
+{
+    public const string errorMessage = "the weight can't be in negative";
+
+    private double weight;
+
+    // TODO: define the 'Precision' property
+    public int Precision { get; }
+
+    // TODO: define the 'Weight' property
+    public double Weight
+    {
+        get { return weight; }
+        set
+        {
+            if (value < 0.0f) throw new ArgumentOutOfRangeException(errorMessage);
+            weight = value;
+        }
+    }
+
+    // TODO: define the 'DisplayWeight' property
+    public string DisplayWeight
+    {
+        get { return $"{(Weight - TareAdjustment).ToString($"N{Precision}")} kg"; }
+    }
+
+    // TODO: define the 'TareAdjustment' property
+    public double TareAdjustment { get; set; }
+
+    public WeighingMachine(int precision)
+    {
+        Precision = precision;
+        TareAdjustment = 5.0f;
+    }
+}
